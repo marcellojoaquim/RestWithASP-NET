ef644dc04040a709c5cf0863e6969ed23e51f2d7
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2026-08-04 22:13:28 -0300

    add migration config

--- ApiRestProject/Program.cs
+++ ApiRestProject/Program.cs
@@ -3,7 +3,10 @@ using ApiRestProject.Business.Impl;
 using ApiRestProject.Model.Context;
 using ApiRestProject.Repository;
 using ApiRestProject.Repository.Impl;
+using EvolveDb;
 using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +14,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
     connection, new MySqlServerVersion(new Version(8, 0, 46))));
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
 
 builder.Services.AddApiVersioning();
 //Injecao de dependencia
@@ -39,3 +48,24 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void MigrateDatabase(string? connection)
+{
+  try
+  {
+    var evolveConnection = new MySqlConnection(connection);
+    var evolve = new Evolve(evolveConnection, Log.Information)
+    {
+        Locations = new List<string> {"db/migrations", "db/dataset"},
+        IsEraseDisabled = true,
+    };
+
+    evolve.Migrate();
+
+  }
+  catch (Exception ex)
+  {
+    Log.Error("Database Migration Error", ex);
+    throw;
+  }
+}
--- ApiRestProject/Repository/Impl/PersonRepositoryImpl.cs
+++ ApiRestProject/Repository/Impl/PersonRepositoryImpl.cs
@@ -57,7 +57,7 @@ public class PersonRepositoryImpl : IPersonRepository
 
   public Person Update(Person person)
   {
-    if (!Exists(person.Id)) return new Person();
+    if (!Exists(person.Id)) return null;
 
     var result = _context.People.SingleOrDefault(p => p.Id.Equals(person.Id));
     if (result != null)
